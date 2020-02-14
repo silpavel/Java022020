a95da9df8de5c282e6052f6833f53314ea48c0d0
silpavel <pasha.nn@mail.ru>
2020-02-14 15:43:22 +0300

    test "clone" method. use class b and class A.

--- src/ru/pasha/first/Main.java
+++ src/ru/pasha/first/Main.java
@@ -1,11 +1,54 @@
 package ru.pasha.first;
+import static java.lang.System.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //test first change
-        //test second change
-        // last test commit and test push
+	    try {
+            A a=new A(10);
+	        B b1=new B(a,"Paul");
+	        B b2=b1.clone();
+	        //out.println(b1.getA().getData());
+	        b2.getA().setData(20);
+	        b2.setName("Victor");
+            out.println(b1.getA().getData());
+            out.println(b1.getName());
+
+        }catch (CloneNotSupportedException cnse){err.println(cnse.toString());}
+
     }
+}
+class A{
+    //methods
+    @Override
+    protected A clone() throws CloneNotSupportedException {
+        return new A(data);
+    }
+    // constructors
+    public A(int data) {this.data = data;}
+    // private data, getters, setters
+    private  int data;
+        public int getData() {return data;}
+        public void setData(int data) {this.data = data;}
+    // private methods
+}
+class B{
+    //methods
+    @Override
+    protected B clone() throws CloneNotSupportedException {
+        return new B(new A(a.getData()), name);
+    }
+    // constructors
+    public B(A a, String name) {
+        this.a = a;
+        this.name=name;
+    }
+    // private data, getters, setters
+    private A a;
+        public A getA() {return a;}
+        public void setA(A a) {this.a = a;}
+    private String name;
+        public String getName() {return name;}
+        public void setName(String name) {this.name = name;}
+    // private methods
 }
